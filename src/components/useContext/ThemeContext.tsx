eb746b301be6ca1640887a9af89249266861b261
Adrian <94838328+Bociarz1@users.noreply.github.com>
2022-11-27 20:13:56 +0100

    Update ThemeContext.tsx

--- src/components/useContext/ThemeContext.tsx
+++ src/components/useContext/ThemeContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext } from "react";
 import {theme} from './theme'
 
 export const ThemeContext = createContext(theme)
+export const useThemeContext = () => useContext(ThemeContext)
 
 type ThemeContextProviderType = {
   children: React.ReactNode
